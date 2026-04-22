4b6142cb50e6207fc8d21cc4ff2844288734c063
arayalashar <arya.alashar2004@gmail.com>
2026-04-22 10:42:30 +0700

    Memperbaru data untuk keperluan hosting

--- koneksi.php
+++ koneksi.php
@@ -1,16 +1,24 @@
 <?php
-// File: koneksi.php
 
-$host = "localhost";
-$user = "root";
-$pass = "";
-$db   = "db_jp";
+// DATA DARI INFINITYFREE
+$host = "sql208.infinityfree.com";
+$user = "if0_41701669";
+$pass = "AZRy77k8ac8 "; 
+$db   = "if0_41701669_db_jp";
 
 // Melakukan koneksi
 $koneksi = mysqli_connect($host, $user, $pass, $db);
 
 // Cek koneksi
 if (!$koneksi) {
-    die("Gagal terhubung ke database: " . mysqli_connect_error());
+    die(json_encode([
+        "status" => "error", 
+        "message" => "Gagal terhubung ke database: " . mysqli_connect_error()
+    ]));
 }
+
+// WAJIB DITAMBAHKAN AGAR FLUTTER BISA MEMBACA DATANYA
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json; charset=UTF-8");
 ?>
